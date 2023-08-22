c2ee29be869ded689624e2a433956092cf85538c
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-08-22 19:05:27 +0530

    fix: ts-errors

--- db/client.ts
+++ db/client.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string)
 
 export const fetchData = async () => {
     const { data, error } = await supabase
